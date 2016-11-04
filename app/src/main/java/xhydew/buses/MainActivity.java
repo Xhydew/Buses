42ff89fbac9766b2dbc1f6cb0a3c5ae685a6cd9a
Joan <sidewinder476@gmail.com>
2016-11-04 09:09:56 +0100

    Primer async

--- app/src/main/java/xhydew/buses/MainActivity.java
+++ app/src/main/java/xhydew/buses/MainActivity.java
@@ -1,11 +1,13 @@
 package xhydew.buses;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import org.apache.commons.io.IOUtils;
@@ -17,14 +19,20 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+    boolean lock = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Chronometer chronometer = (Chronometer) findViewById(R.id.Main_chronometer);
+        assert chronometer != null;
+       // chronometer.setFormat("00:%s");
+
     }
 
     public ArrayList<Bus> getBuses() {
@@ -33,42 +41,66 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         try {
-            HttpURLConnection con = (HttpURLConnection) new URL("http://www.infobuselche.es/ws/paradas/"+numPar.getText().toString()+".php").openConnection();
+            HttpURLConnection con = (HttpURLConnection) new URL("http://www.infobuselche.es/ws/paradas/" + (numPar != null ? numPar.getText().toString() : "1") + ".php").openConnection();
             String json = IOUtils.toString(con.getInputStream(), con.getContentEncoding());
 
             JSONObject jsonObject = new JSONObject(json);
-            String j = jsonObject.toString();
-            System.out.println(j);
             jsonObject = jsonObject.getJSONObject("GetPasoParadaResult");
             JSONArray jsonArr = jsonObject.getJSONArray("PasoParada");
             for (int i = 0; i < jsonArr.length(); i++) {
                 JSONObject linea = jsonArr.getJSONObject(i);
                 String lineaStr = linea.getString("linea");
-                JSONObject bus1 = linea.getJSONObject("e1");
-                JSONObject bus2 = linea.getJSONObject("e2");
-                int min = bus1.getInt("minutos");
-                int min2 = bus2.getInt("minutos");
-                buses.add(new Bus(min, 1, 1, lineaStr));
-                buses.add(new Bus(min2, 1, 1, lineaStr));
+                for (int j = 1; j <= 2; j++) {
+                    JSONObject bus1 = linea.getJSONObject("e" + j);
+                    int min = bus1.getInt("minutos");
+                    int mtr = bus1.getInt("metros");
+                    buses.add(new Bus(min, 1, mtr, lineaStr));
+                }
             }
-        } catch (IOException e) {
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
 
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
         return buses;
     }
 
     public void click(View v) {
-        TextView texto = (TextView) findViewById(R.id.Main_Texto_1);
-        String strBuses = "";
-        ArrayList<Bus> buses = getBuses();
-        Iterator<Bus> it = buses.iterator();
-        while (it.hasNext()) {
-            Bus bus = it.next();
-            strBuses += bus.getLinea() + "\t\t--\t\t" + bus.getTiempo()  + "\n";
+        if (!lock) {
+            lock = true;
+            findViewById(R.id.Main_Relative).setBackgroundColor(Color.BLACK);
+            final Chronometer chronometer = (Chronometer) findViewById(R.id.Main_chronometer);
+            assert chronometer != null;
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            chronometer.start();
+            //findViewById(R.id.Main_chronometer).;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    String strBuses = "";
+                    ArrayList<Bus> buses = getBuses();
+                    for (Bus bus : buses) {
+                        strBuses += bus.getLinea() + "\t\t--\t\t" + bus.getTiempo() + "\t\t--\t\t" + bus.getMetros() + " m\n";
+                    }
+                    final String finalStrBuses = strBuses;
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView texto = (TextView) findViewById(R.id.Main_Texto_1);
+
+                            assert texto != null;
+                            texto.setText(finalStrBuses);
+                            findViewById(R.id.Main_Relative).setBackgroundColor(Color.BLUE);
+                            chronometer.stop();
+                        }
+                    });
+                }
+            }, "GET parada").start();
+
+
+            lock = false;
         }
-        texto.setText(strBuses);
     }
 }
